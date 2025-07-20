bad5922b5fa6b5354eb04ae8d4d8e50a242403f2
luvkumar404 <lovechaudhary1090@gmail.com>
2025-07-20 16:22:53 +0530

    Finished

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,20 +1,19 @@
 import express from "express";
 import { createServer } from "node:http";
-
 import { Server } from "socket.io";
-
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 import { connectToSocket } from "./controllers/socketManager.js";
-
 import cors from "cors";
 import userRoutes from "./routes/users.routes.js";
+
+dotenv.config(); // Load environment variables
 
 const app = express();
 const server = createServer(app);
 const io = connectToSocket(server);
 
-
-app.set("port", (process.env.PORT || 8000))
+app.set("port", process.env.PORT || 8000);
 app.use(cors());
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
@@ -22,18 +21,11 @@ app.use(express.urlencoded({ limit: "40kb", extended: true }));
 app.use("/api/v1/users", userRoutes);
 
 const start = async () => {
-    app.set("mongo_user")
-    const connectionDb = await mongoose.connect("mongodb+srv://ZoomClone:ZoomClone404@zoomclone.3wotguc.mongodb.net/")
-
-    console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
+    const connectionDb = await mongoose.connect(process.env.MONGO_URL);
+    console.log(`MONGO Connected DB Host: ${connectionDb.connection.host}`);
     server.listen(app.get("port"), () => {
-        console.log("LISTENIN ON PORT 8000")
+        console.log(`LISTENING ON PORT ${app.get("port")}`);
     });
-
-
-
-}
-
-
+};
 
 start();
